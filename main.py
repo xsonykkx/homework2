b7925d4246dbb562a0880b1bc3b9c29d3ec5b09c
xsonykkx <sofiya.strologo@gmail.com>
2024-07-04 13:07:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+name_of_the_course = 'Python'
+tasks_in_total = 12
+time_spent_in_hours = 1.5
+execution_time_on_average = time_spent_in_hours / tasks_in_total
+print (name_of_the_course, tasks_in_total, time_spent_in_hours, execution_time_on_average)
